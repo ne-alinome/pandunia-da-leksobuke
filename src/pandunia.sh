ec84275e4838f46e9f2cd3a10e62ad1c9f7db46c
Marcos Cruz <github@programandala.net>
2019-01-18 14:08:17 +0100

    Add a dict wrapper

---
+++ src/pandunia.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# pandunia
+
+# A dict wrapper to search the Pandunia dictionary.
+
+# This file is part of the project
+# _pandunia da lekse buke_
+# (http://ne.alinome.net)
+
+# By Marcos Cruz (programandala.net), 2019
+
+# 2019-01-11
+
+dict -d pandunia $@
