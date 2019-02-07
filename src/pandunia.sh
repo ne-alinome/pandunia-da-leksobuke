141a37539c3ced29368134db27984816d756504c
Marcos Cruz <github@programandala.net>
2019-02-07 17:14:30 +0100

    Make the DICT wrapper search all dictionaries

--- src/pandunia.sh
+++ src/pandunia.sh
@@ -10,6 +10,25 @@
 
 # By Marcos Cruz (programandala.net), 2019
 
-# 2019-01-11
 
-dict -d pandunia $@
+# Usage
+#
+# Copy or link this file to a directory in your path, with your name
+# of choice, e.g.  <~/bin/pandunia>, <~/.local/bin/pandunia> or
+# </usr/local/bin/pandunia>.
+
+# Change log
+#
+# 2019-01-11: First version, only for the main dictionary.
+# 2019-02-07: Search all Pandunia dictionaries installed.
+
+dictionaries=$(\
+  dict -D | \
+  grep "\<pandunia\>" --ignore-case |\
+  sed -e "s/\s*\(\S\+\) .*/\1/"
+  )
+
+for dictionary in $dictionaries
+do
+  dict -d $dictionary $*
+done
